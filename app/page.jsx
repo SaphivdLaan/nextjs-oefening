3f507951356d05b047bc9aaecde632366cf014fd
SaphivdLaan <Sooff@icloud.com>
2025-04-08 11:02:46 +0200

    aanpassing? waarom werkt dit niet? help?

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ export default function Home() {
     <main className="py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto text-center">
         <h2 className="text-4xl font-semibold text-indigo-600 mb-6">
-          What is Next.js?
+          What is Next?
         </h2>
         <p className="text-lg text-gray-700 mb-8">
           Next.js is a React-based framework that enables server-side rendering
